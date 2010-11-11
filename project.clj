6c4d50451cf6962a685beabaab2a4b662b70a3c8
Lau B. Jensen <lau.jensen@bestinclass.dk>
2010-11-11 22:59:19 +0100

    Updated swank version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies     [[org.clojure/clojure         "1.2.0"]
                      [org.clojure/clojure-contrib "1.2.0"]
                      [mysql/mysql-connector-java  "5.1.6"]]
-  :dev-dependencies [[swank-clojure               "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure               "1.3.0-SNAPSHOT"]])
 
