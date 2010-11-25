b5e04d7ed7ef0dad4d9f37a741c87784bf23fdfd
Roman Scherer <roman@burningswell.com>
2010-11-26 00:29:30 +0100

    Added postgresql and sqlite3 drivers to dev dependencies.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :dependencies     [[org.clojure/clojure         "1.2.0"]
                      [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure               "1.3.0-SNAPSHOT"]
-                     [mysql/mysql-connector-java  "5.1.6"]])
+                     [mysql/mysql-connector-java  "5.1.6"]
+                     [org.xerial/sqlite-jdbc "3.6.20.1"]
+                     [postgresql/postgresql "8.4-701.jdbc4"]])
 
