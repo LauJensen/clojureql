5f515fb881031a8573f2e164042ff4ea93145c74
bendlas <hhochleitner@gmail.com>
2011-07-29 03:07:56 +0200

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clojureql "1.1.0-SNAPSHOT"
   :description      "Superior SQL integration for Clojure"
-  :dependencies     [[org.clojure/clojure         "1.2.0"]
+  :dependencies     [[org.clojure/clojure         "1.2.1"]
                      [org.clojure/core.incubator  "0.1.0"]
                      [org.clojure.contrib/mock    "1.3.0-alpha4"]
                      [org.clojure/java.jdbc       "0.0.5"]]
   :dev-dependencies [[swank-clojure               "1.3.0-SNAPSHOT"]
-                     [mysql/mysql-connector-java  "5.1.6"]
-                     [org.xerial/sqlite-jdbc      "3.6.20.1"]
-                     [postgresql/postgresql       "8.4-701.jdbc4"]]
+                     [mysql/mysql-connector-java  "5.1.17"]
+                     [org.xerial/sqlite-jdbc      "3.7.2"]
+                     [postgresql/postgresql       "8.4-702.jdbc4"]]
 
   :repositories {"clojure-releases"  {:url "http://build.clojure.org/releases"}})
 
