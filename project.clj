16ecfb9b7b89bdaadfe6c90ef16cd9b7e6cafdd2
Andreas Wurzer <andreas.wurzer@gmail.com>
2011-08-25 21:35:09 +0200

    added old hsqldb to test against (jdbc2 driver)

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
                      [org.clojure/core.incubator  "0.1.0"]
                      [org.clojure.contrib/mock    "1.3.0-alpha4"]
                      [org.clojure/java.jdbc       "0.0.5"]]
-  :dev-dependencies [[swank-clojure               "1.3.0-SNAPSHOT"]
+  :dev-dependencies [[swank-clojure               "1.3.2"]
                      [mysql/mysql-connector-java  "5.1.17"]
                      [org.xerial/sqlite-jdbc      "3.7.2"]
-                     [postgresql/postgresql       "8.4-702.jdbc4"]]
+                     [postgresql/postgresql       "8.4-702.jdbc4"]
+                     [hsqldb/hsqldb               "1.6.1"]]
 
   :repositories {"clojure-releases"  {:url "http://build.clojure.org/releases"}})
 
