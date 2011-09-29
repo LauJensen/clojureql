26c4d4e0fee7aa9daebcae63621a0bcf1f0ac49d
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-09-29 15:54:36 +0100

    fix join order problem for join paths >= 2 with subselects and table aliases

--- test/clojureql/test/internal.clj
+++ test/clojureql/test/internal.clj
@@ -30,3 +30,120 @@
     "user" "user"
     {:user :developer} "developer"
     {"user" "developer"} "developer"))
+
+(deftest test-table-alias
+  (are [tname expected]
+       (is (= expected (table-alias tname)))
+       :user "user"
+       "user" "user"
+       {:user :u} "u"))
+
+(deftest test-subselect-table-alias
+  (are [tname expected]
+       (is (= expected (subselect-table-alias tname)))
+       :user "user_subselect"
+       "user" "user_subselect"
+       {:user :u} "u"))
+
+(deftest test-join-table-alias
+  (are [table-name-alias expected]
+       (is (= expected (join-table-alias table-name-alias)))
+       "user" "user"
+       "user u" "u"
+       {:tname :user} "user_subselect"))
+
+(deftest test-joins-by-table-alias
+  (are [joins expected]
+       (is (= expected (joins-by-table-alias joins)))
+
+       ;; simple case : join to an unaliased table
+       [{:data ["user" {:cols ["user.company_id" "companies.id"]}]}] 
+       {"user" {:data ["user" {:cols ["user.company_id" "companies.id"]}]}}
+
+       ;; join to an aliased table
+       [{:data ["user u" {:cols ["u.company_id" "companies.id"]}]}] 
+       {"u" {:data ["user u" {:cols ["u.company_id" "companies.id"]}]}}
+
+       ;; join to a subselect
+       [{:data [{:tname :user} {:cols ["user.company_id" "companies.id"]}]}] 
+       {"user_subselect" {:data [{:tname :user} {:cols ["user.company_id" "companies.id"]}]}}
+
+       ;; join to a subselect with explicit alias
+       [{:data [{:tname {:user :u}} {:cols ["u.company_id" "companies.id"]}]}] 
+       {"u" {:data [{:tname {:user :u}} {:cols ["u.company_id" "companies.id"]}]}}
+
+       ;; multiple joins
+       [{:data ["user" {:cols ["user.company_id" "companies.id"]}]}
+        {:data ["hats" {:cols ["hats.id" "user.hat_id"]}]}]
+       {"user" {:data ["user" {:cols ["user.company_id" "companies.id"]}]}
+        "hats" {:data ["hats" {:cols ["hats.id" "user.hat_id"]}]}}))
+
+(deftest test-join-column-names
+  (are [table-name-alias cols expected]
+       (is (= expected (join-column-names table-name-alias cols)))
+       
+       ;; simple case : unaliased table
+       "user" ["user.company_id" "companies.id"] ["user.company_id" "companies.id"]
+       
+       ;; simple case : aliased table
+       "user u" ["u.company_id" "companies.id"] ["u.company_id" "companies.id"]
+       
+       ;; subselect
+       {:tname :user} ["user.company_id" "companies.id"] ["user_subselect.company_id" "companies.id"]
+       
+       ;; subselect with expicit alias
+       {:tname {:user :u}} ["u.company_id" "companies.id"] ["u.company_id" "companies.id"]))
+
+(deftest test-to-tbl-name
+  (are [join expected]
+       (is (= expected (to-tbl-name join)))
+
+       ;; join to an unaliased table
+       {:data ["user" {:cols ["user.company_id" "companies.id"]}]} "companies"
+       
+       ;; join to an aliased table
+       {:data ["user u" {:cols ["u.company_id" "companies.id"]}]} "companies"
+       
+       ;; join to a subselect
+       {:data [{:tname :user} {:cols ["user.company_id" "companies.id"]}]} "companies"
+       
+       ;; join to an explicitly aliased subselect
+       {:data [{:tname {:user :u}} {:cols ["u.company_id" "companies.id"]}]} "companies"))
+
+(deftest test-to-graph-el
+  (are [m join expected]
+       (is (= expected (to-graph-el m join)))
+
+       ;; join to an unaliased table
+       {} {:data ["user" {:cols ["user.company_id" "companies.id"]}]} {"user" "companies"}
+
+       ;; join to an aliased table
+       {} {:data ["user u" {:cols ["u.company_id" "companies.id"]}]} {"u" "companies"}
+       
+       ;; join to a subselect
+       {} {:data [{:tname :user} {:cols ["user.company_id" "companies.id"]}]} {"user_subselect" "companies"}
+       
+       ;; join to an explicitly aliased subselect
+       {} {:data [{:tname {:user :u}} {:cols ["u.company_id" "companies.id"]}]} {"u" "companies"}))
+
+(deftest test-add-deps
+  (are [map-of-joins edges tbl expected]
+       (is (= expected (add-deps map-of-joins edges tbl)))
+       
+       ;; deps for a from a join b join c 
+       {"b" :b-join "c" :c-join}
+       {"b" "a" "c" "b"}
+       "a"
+       [nil [:b-join [:c-join]]]
+       
+       ;; deps for b from a join b join c
+       {"b" :b-join "c" :c-join}
+       {"b" "a" "c" "b"}
+       "b"
+       [:b-join [:c-join]]
+       
+       ;; deps for c from a join b join c
+       {"b" :b-join "c" :c-join}
+       {"b" "a" "c" "b"}
+       "c"
+       [:c-join]))
