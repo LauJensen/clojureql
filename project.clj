862ed4472c13b025e48c132ca4cf7a97afe0e9d3
Don Jackson <dcj-github@clark-communications.com>
2011-12-06 12:19:09 -0800

    Please consider upgrading java.jdbc version to 0.1.1 Of course, this will require testing first...

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies     [[org.clojure/clojure         "1.2.1"]
                      [org.clojure/core.incubator  "0.1.0"]
                      [org.clojure.contrib/mock    "1.3.0-alpha4"]
-                     [org.clojure/java.jdbc       "0.0.5"]]
+                     [org.clojure/java.jdbc       "0.1.1"]]
   :dev-dependencies [[mysql/mysql-connector-java  "5.1.17"]
                      [org.xerial/sqlite-jdbc      "3.7.2"]
                      [postgresql/postgresql       "8.4-702.jdbc4"]
